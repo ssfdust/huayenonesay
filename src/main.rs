8a6c16bce47fdac808c5c9690e5431483cc90e53
ssfdust <ssfdust@gmail.com>
2022-03-31 14:11:44 +0800

    Add cache check

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,12 @@ struct BgArgs {
     device: String,
 }
 
+#[derive(Default, Debug)]
+struct Cached {
+    onesay: String,
+    chapter: String,
+    date: String
+}
 
 #[get("/api/huayen/onesay")]
 async fn onesay(session: Session) -> impl Responder {
@@ -22,14 +28,26 @@ async fn onesay(session: Session) -> impl Responder {
     let chapter: String;
     let local: DateTime<Local> = Local::now();
     let datenowstr: String = local.format("%Y%m%d").to_string();
-    match session.get::<String>("date") {
-        Ok(Some(date)) if date.eq(&datenowstr) => {
-            onesay = session.get::<String>("onesay").unwrap().unwrap();
-            chapter = session.get::<String>("chapter").unwrap().unwrap();
+    let mut cached = Cached::default();
+    for (key, val) in session.entries().iter() {
+        if key.eq("onesay") {
+            cached.onesay = val.replace("\"", "").to_owned()
+        }
+        if key.eq("date") {
+            cached.date = val.replace("\"", "").to_owned()
+        }
+        if key.eq("chapter") {
+            cached.chapter = val.replace("\"", "").to_owned()
+        }
+    }
+    match cached {
+        cached if cached.date.eq(&datenowstr) && cached.onesay.len() > 0 && cached.chapter.len() > 0 => {
+            onesay = cached.onesay.to_owned();
+            chapter = cached.chapter.to_owned();
         }
         _ => {
             (onesay, chapter) = get_random_saying();
-            session.insert("date", datenowstr).unwrap();
+            session.insert("date", &datenowstr).unwrap();
             session.insert("chapter", &chapter).unwrap();
             session.insert("onesay", &onesay).unwrap();
         }
